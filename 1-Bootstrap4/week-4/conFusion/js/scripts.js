530e96318718624def70f33dd29daaa08df9f955
Christian Zuluaga <christian.zuluaga247@gmail.com>
2017-09-27 16:40:06 -0500

    Fix some style

--- 1-Bootstrap4/week-4/conFusion/js/scripts.js
+++ 1-Bootstrap4/week-4/conFusion/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("#mycarousel").carousel({ interval: 1000 });
+    $("#mycarousel").carousel({ interval: 3000 });
     $("#carousel-button").click(function () {
         if ($("#carousel-button").children("span").hasClass("fa-pause")) {
             $("#mycarousel").carousel('pause');
